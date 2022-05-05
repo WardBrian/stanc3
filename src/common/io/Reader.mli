9e194d26c8001ce8f6c91a9de06c3084aae8d70f
Brian Ward <bward@flatironinstitute.org>
2022-05-05 12:36:46 -0400

    Notes

--- src/common/io/Reader.mli
+++ src/common/io/Reader.mli
@@ -1,1 +1,8 @@
 val read : string -> string list option
+
+(* above should be read_lines or just return string option
+   should also define a
+   lexbuf_of fname
+
+   Do we ever need file writing? I don't think so
+*)
