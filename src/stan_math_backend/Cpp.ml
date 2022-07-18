5d0b286b78a0009881ab88c588b57174e1ca9086
Brian Ward <bward@flatironinstitute.org>
2022-07-18 10:15:12 -0400

    Keep trying

--- src/stan_math_backend/Cpp.ml
+++ src/stan_math_backend/Cpp.ml
@@ -1,32 +1,20 @@
 open! Core_kernel
-open Fmt
 
-type template =
-  | Typename of string
+type template_parameter =
+  | Typename of string  (** The name of a template typename *)
   | Require of string * string
-  | Bool of string
-
-let pp_template ppf template =
-  match template with
-  | Typename t -> pf ppf "typename %s" t
-  | Require (r, t) -> pf ppf "%s<%s>*" r t
-  | Bool s -> pf ppf "bool %s" s
-
-let pp_template_defaults ppf template =
-  match template with
-  | Require _ -> pf ppf "%a = nullptr" pp_template template
-  | _ -> pp_template ppf template
-
-let pp_templates ~defaults ppf templates =
-  match templates with
-  | [] -> ()
-  | _ ->
-      pf ppf "template <@[%a@]>@ "
-        (list ~sep:comma
-           (if defaults then pp_template_defaults else pp_template) )
-        templates
+      (** A C++ type trait and the name which needs to satisfy that *)
+  | Bool of string  (** A named boolean template type *)
 
 type identifier = string
+
+type expr =
+  | Literal of string
+  | Var of identifier
+  | FunCall of identifier * expr list
+  | MethodCall of expr * identifier * expr list
+  | Assign of identifier * expr
+  | Incr of identifier
 
 type type_ =
   | Auto
@@ -37,16 +25,7 @@ type type_ =
   | Vector of type_
   | Class of identifier
   | Matrix of type_ * int * int
-
-type return_ty = Type of type_ | ReturnType of type_ list
-
-type expr =
-  | Literal of string
-  | Var of identifier
-  | FunCall of identifier * expr list
-  | MethodCall of expr * identifier * expr list
-  | Assign of identifier * expr
-  | Incr of identifier
+  | TypeTrait of string * type_ list
 
 type var_defn =
   { const: bool [@default false]
@@ -59,12 +38,17 @@ type stmt =
   | Expression of expr
   | VarDef of var_defn
   | For of expr * expr * expr * stmt
+  | While of expr * stmt
+  | IfElse of expr * stmt * stmt
   | Block of stmt list
+  | Comment of string
+
+type return_ty = Type of type_ | Void
 
 type fun_defn =
   { inline: bool [@default false]
   ; const: bool [@default false]
-  ; templates: template list [@default []]
+  ; templates: template_parameter list [@default []]
   ; name: identifier
   ; return_type: return_ty
   ; args: type_ * string list
@@ -73,11 +57,62 @@ type fun_defn =
 
 type defn =
   | FunDef of fun_defn
-  | Struct of identifier * defn list
+  | Struct of template_parameter option * identifier * defn list
   | TopVarDef of var_defn
+  | TopComment of string
+  | Using of string
+  | Namespace of string * defn list
+
+type program = defn list
 
 type found_functor =
-  { struct_template: template option
-  ; arg_templates: template list
+  { struct_template: template_parameter option
+  ; arg_templates: template_parameter list
   ; signature: string
   ; defn: string }
+
+module Printing = struct
+  open Fmt
+
+  let pp_template_parameter ppf template_parameter =
+    match template_parameter with
+    | Typename param_name -> pf ppf "typename %s" param_name
+    | Require (requirement, param_name) ->
+        pf ppf "stan::require_t<%s<%s>>*" requirement param_name
+    | Bool param_name -> pf ppf "bool %s" param_name
+
+  let pp_requires ~default ppf requires =
+    match requires with
+    | [] -> ()
+    | _ ->
+        let pp_require ppf (trait, name) = pf ppf "%s<%s>" trait name in
+        pf ppf ",@ stan::require_all_t<@[%a@]>*%s"
+          (list ~sep:comma pp_require)
+          requires
+          (if default then " = nullptr" else "")
+
+  (**
+   Pretty print a full C++ `template <parameter-list>`
+  *)
+  let pp_template ~default ppf template_parameters =
+    match template_parameters with
+    | [] -> ()
+    | _ ->
+        let templates, requires =
+          List.partition_map template_parameters ~f:(function
+            | Require (trait, name) -> Second (trait, name)
+            | Typename name -> First ("typename " ^ name)
+            | Bool name -> First ("bool " ^ name) ) in
+        pf ppf "template <@[%a%a@]>@ " (list ~sep:comma string) templates
+          (pp_requires ~default) requires
+
+  let rec pp_expr ppf e =
+    match e with
+    | Literal s -> pf ppf "@[%a@]" text s
+    | Var id -> string ppf id
+    | FunCall (fn, es) -> pf ppf "%s(@[%a@])" fn (list pp_expr) es
+    | MethodCall (e, fn, es) ->
+        pf ppf "%a.%s(@[%a@])" pp_expr e fn (list pp_expr) es
+    | Assign (id, e) -> pf ppf "%s = %a" id pp_expr e
+    | Incr id -> pf ppf "++%s" id
+end
