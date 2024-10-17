8e51399a0583988a3e6523f4cc9930384f038b14
Brian Ward <bward@flatironinstitute.org>
2024-10-17 11:49:20 -0400

    update

--- mutaml.sh
+++ mutaml.sh
@@ -2,5 +2,5 @@
 
 rm -rf _build/.lock || true
 find _build/default/test -name '*.output' -delete || true
-dune runtest test/integration/ --force
+dune runtest test/integration/ --force -j 12
 exit $?
