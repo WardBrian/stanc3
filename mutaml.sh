3a523964312046e482b3c80e5ee9a98f6958b76b
Brian Ward <brianmward99@gmail.com>
2024-11-11 11:51:09 -0500

    stop-on-first-error

--- mutaml.sh
+++ mutaml.sh
@@ -2,5 +2,5 @@
 
 rm -rf _build/.lock || true
 find _build/default/test -name '*.output' -delete || true
-dune runtest test/integration/ --force -j 12
+dune runtest test/integration/ --force -j 12 --stop-on-first-error
 exit $?
