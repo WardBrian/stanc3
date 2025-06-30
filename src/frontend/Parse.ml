dbbe0379670101692d818c7b75686ddb2c8dbba8
Brian Ward <bward@flatironinstitute.org>
2025-06-30 12:48:39 -0400

    Try code backend

--- src/frontend/Parse.ml
+++ src/frontend/Parse.ml
@@ -3,41 +3,39 @@
 
 open Core
 open Common.Let_syntax.Result
-module Interp = Parser.MenhirInterpreter
 
 let drive_parser parse_fun =
-  let input () =
-    Interp.lexer_lexbuf_to_supplier Lexer.token
-      (Preprocessor.current_buffer ())
-      () in
-  let success prog = Ok {prog with Ast.comments= Preprocessor.get_comments ()} in
-  let failure error_state =
-    (* see the Menhir manual for the description of
-       error messages support *)
-    let env =
-      match error_state with
-      | Interp.HandlingError env -> env
-      | _ ->
+  let lex in_buf =
+    let real_buf = Preprocessor.current_buffer () in
+    let tok = Lexer.token real_buf in
+    (* TODO -- not really working, I think this needs to check if the tokenizer
+      changed buffers due to #includes *)
+    in_buf.Lexing.lex_start_p <- (Preprocessor.current_buffer ()).lex_start_p;
+    in_buf.Lexing.lex_curr_p <- real_buf.lex_curr_p;
+    tok in
+  try
+    let prog = parse_fun lex (Preprocessor.current_buffer ()) in
+    Ok {prog with Ast.comments= Preprocessor.get_comments ()}
+  with
+  | Errors.SyntaxError err -> Result.Error (Errors.Syntax_error err)
+  | Parser.Error state ->
+      let message =
+        try
+          Fmt.str "%s%a"
+            (Parsing_errors.message state)
+            (Fmt.if' !Debugging.grammar_logging (fun ppf ->
+                 Fmt.pf ppf "(Parse error state %d)"))
+            state
+        with _ ->
           Common.ICE.internal_compiler_error
-            [%message "Parser failed but is not in an error state "] in
-    let message =
-      let state = Interp.current_state_number env in
-      try
-        Fmt.str "%s%a"
-          (Parsing_errors.message state)
-          (Fmt.if' !Debugging.grammar_logging (fun ppf ->
-               Fmt.pf ppf "(Parse error state %d)"))
-          state
-      with _ ->
-        Common.ICE.internal_compiler_error
-          [%message
-            "Failed to find error for parser error state " (state : int)] in
-    let location =
-      Preprocessor.location_span_of_positions (Interp.positions env) in
-    Error (Errors.Syntax_error (Errors.Parsing (message, location))) in
-  let startp = (Preprocessor.current_buffer ()).lex_curr_p in
-  try Interp.loop_handle success failure input (parse_fun startp)
-  with Errors.SyntaxError err -> Result.Error (Errors.Syntax_error err)
+            [%message
+              "Failed to find error for parser error state " (state : int)]
+      in
+      let location =
+        Preprocessor.location_span_of_positions
+          ( Lexing.lexeme_start_p (Preprocessor.current_buffer ())
+          , Lexing.lexeme_end_p (Preprocessor.current_buffer ()) ) in
+      Error (Errors.Syntax_error (Errors.Parsing (message, location)))
 
 let to_lexbuf file_or_code =
   match file_or_code with
@@ -56,7 +54,5 @@ let parse parse_fun file_or_code =
     drive_parser parse_fun in
   (result, Input_warnings.collect ())
 
-let parse_stanfunctions file_or_code =
-  parse Parser.Incremental.functions_only file_or_code
-
-let parse_program file_or_code = parse Parser.Incremental.program file_or_code
+let parse_stanfunctions file_or_code = parse Parser.functions_only file_or_code
+let parse_program file_or_code = parse Parser.program file_or_code
