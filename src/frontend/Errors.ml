026b7c44b0ca08453f752ca6b818752109640d91
Brian Ward <bward@flatironinstitute.org>
2026-02-19 15:45:51 -0500

    threading args

--- src/frontend/Errors.ml
+++ src/frontend/Errors.ml
@@ -9,7 +9,6 @@ type t =
   | DebugDataError of (Middle.Location_span.t * string * bool)
 
 let get_context ?code Middle.Location.{filename; included_from; _} =
-  Option.try_with @@ fun () ->
   match (included_from, code) with
   | None, Some code ->
       (* If the location is not included from anywhere, and we have code
@@ -28,26 +27,32 @@ let get_context ?code Middle.Location.{filename; included_from; _} =
 
 let red = Fmt.(styled `Bold (styled (`Fg `Red) string))
 
-let get_source ?code loc =
-  let code = get_context ?code loc |> Option.value ~default:[] in
+let get_source ?printed_filename ?code loc =
+  let code = get_context ?code loc in
   let source : Grace.Source.t =
-    `String {name= Some loc.filename; content= code |> String.concat ~sep:"\n"}
-  in
+    `String
+      { name=
+          (if Option.is_none loc.included_from then
+             Option.first_some printed_filename (Some loc.filename)
+           else Some loc.filename)
+      ; content= code |> String.concat ~sep:"\n" } in
   source
 
-let range_of_loc_span ?code ({begin_loc; end_loc} : Middle.Location_span.t) =
+let range_of_loc_span ?printed_filename ?code
+    ({begin_loc; end_loc} : Middle.Location_span.t) =
   let open Grace in
-  let source = get_source ?code begin_loc in
+  let source = get_source ?printed_filename ?code begin_loc in
   let start = begin_loc.bol_offset + begin_loc.col_num in
   let end_ = end_loc.bol_offset + end_loc.col_num in
   let end_ = Int.clamp_exn ~min:start ~max:(Source.length source) end_ in
   Range.create ~source (Byte_index.of_int start) (Byte_index.of_int end_)
 
-let rec included_diagnostic Middle.Location.{included_from; filename; _} :
-    Grace.Diagnostic.Label.t list =
+let rec included_diagnostic ?printed_filename ?code
+    Middle.Location.{included_from; filename; _} : Grace.Diagnostic.Label.t list
+    =
   let open Grace in
   let range_of_loc loc =
-    let source = get_source loc in
+    let source = get_source ?printed_filename ?code loc in
     let start = loc.bol_offset + loc.col_num in
     let end_ =
       start + 8
@@ -62,9 +67,9 @@ let rec included_diagnostic Middle.Location.{included_from; filename; _} :
       let label =
         Diagnostic.Label.secondaryf ~range:(range_of_loc loc)
           "file '%s' included here" filename in
-      label :: included_diagnostic loc
+      label :: included_diagnostic ?printed_filename ?code loc
 
-let syntax_error_to_grace ?code err =
+let syntax_error_to_grace ?printed_filename ?code err =
   let loc_span = Syntax_error.location err in
   Grace.Diagnostic.(
     (* We'd need more locations to really do much -- hard from parser, easy from
@@ -73,21 +78,21 @@ let syntax_error_to_grace ?code err =
       ~labels:
         Label.(
           [ primaryf
-              ~range:(range_of_loc_span ?code loc_span)
+              ~range:(range_of_loc_span ?printed_filename ?code loc_span)
               "%a" Syntax_error.pp err ]
-          @ included_diagnostic loc_span.begin_loc)
+          @ included_diagnostic ?printed_filename ?code loc_span.begin_loc)
       Error "%a" Syntax_error.pp err)
 
-let semantic_error_to_grace ?code err =
+let semantic_error_to_grace ?printed_filename ?code err =
   let loc_span = Semantic_error.location err in
   Grace.Diagnostic.(
     createf
       ~labels:
         Label.(
           [ primaryf
-              ~range:(range_of_loc_span ?code loc_span)
+              ~range:(range_of_loc_span ?printed_filename ?code loc_span)
               "%a" Semantic_error.pp err ]
-          @ included_diagnostic loc_span.begin_loc)
+          @ included_diagnostic ?printed_filename ?code loc_span.begin_loc)
       Error "%a" Semantic_error.pp err)
 
 let pp_diagnostic ppf diagnostic =
